cfebf0e28c2420a69a4020cee6de283f1b9ee092
ValoXTech <valoxblood@gmail.com>
2015-03-28 14:44:26 +0100

    Updated for Android 5.1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,130 +1,163 @@
-# inherit from the proprietary version
--include vendor/oppo/find7a/BoardConfigVendor.mk
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-COMMON_PATH := device/oppo/msm8974-common
+# inherit from Oppo common
+-include device/oppo/common/BoardConfigCommon.mk
 
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := device/oppo/msm8974-common/include
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8974
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
 
 # Architecture
-TARGET_CPU_VARIANT := krait
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_SMP := true
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := krait
+#TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
-# Krait optimizations
-TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+# Assertions
+TARGET_BOARD_INFO_FILE ?= device/oppo/msm8974-common/board-info.txt
 
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.selinux=permissive androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+# Kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/oppo/msm8974-common/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
-BOARD_CUSTOM_BOOTIMG_MK := $(COMMON_PATH)/mkbootimg.mk
+TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
+TARGET_KERNEL_ARCH := arm
 
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00F00000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00F00000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1388314624
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472
-BOARD_FLASH_BLOCK_SIZE := 131072
+# Enable DIAG on debug builds
+ifneq ($(TARGET_BUILD_TYPE),user)
+TARGET_KERNEL_ADDITIONAL_CONFIG:= cyanogenmod_debug_config
+endif
 
-# global
-TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
+# Flags
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+
+# QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_USES_QCOM_BSP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
 
-# Display
-TARGET_QCOM_DISPLAY_VARIANT := caf-bfam
-USE_OPENGL_RENDERER := true
-TARGET_USES_ION := true
-TARGET_USES_C2D_COMPOSITION := true
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
-# Time Daemon
-BOARD_USES_QC_TIME_SERVICES := true
-
-# Audio/media
-TARGET_QCOM_AUDIO_VARIANT := caf-bfam
-TARGET_QCOM_MEDIA_VARIANT := caf-bfam
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-
-# audio
-BOARD_USES_ALSA_AUDIO:= true
-AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
-AUDIO_FEATURE_ENABLED_EXTN_FORMATS := true
-AUDIO_FEATURE_ENABLED_EXTN_POST_PROC := true
-AUDIO_FEATURE_ENABLED_FLUENCE := true
-AUDIO_FEATURE_ENABLED_HFP := true
-AUDIO_FEATURE_ENABLED_PCM_OFFLOAD := true
-AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
-AUDIO_FEATURE_ENABLED_USBAUDIO := true
-AUDIO_FEATURE_ENABLED_COMPRESS_CAPTURE := true
-
-# TODO - if we merge all CAF changes into av we should be able to use the policy there
-USE_LEGACY_AUDIO_POLICY := 1
-USE_CUSTOM_AUDIO_POLICY := 0
-BOARD_USES_EXTN_AUDIO_POLICY_MANAGER := true
-
-# Camera
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DOPPO_CAMERA_HARDWARE
-USE_DEVICE_SPECIFIC_CAMERA := true
-
-# Disable secure discard because it's SLOW
-BOARD_SUPPRESS_SECURE_ERASE := true
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+AUDIO_FEATURE_ENABLED_HWDEP_CAL := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
+QCOM_BT_USE_SMD_TTY := true
 BLUETOOTH_HCI_USE_MCT := true
+
+# Charger
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+
+# CM Hardware
+BOARD_HARDWARE_CLASS += device/oppo/msm8974-common/cmhw
+
+# Graphics
+BOARD_EGL_CFG := device/oppo/msm8974-common/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+HAVE_ADRENO_SOURCE:= false
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
 BOARD_WLAN_DEVICE                := qcwcn
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME          := "wlan"
+TARGET_USES_WCNSS_CTRL           := true
+TARGET_USES_QCOM_WCNSS_QMI       := true
+TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 
-BOARD_EGL_CFG := $(COMMON_PATH)/configs/egl.cfg
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Compatibility with pre-kitkat Qualcomm sensor HALs
-SENSORS_NEED_SETRATE_ON_ENABLE := true
+# No old RPC for prop
+TARGET_NO_RPC := true
 
-# charger
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
+# GPS HAL lives here
+TARGET_GPS_HAL_PATH := device/oppo/msm8974-common/gps
+TARGET_PROVIDES_GPS_LOC_API := true
 
-# power hal
-TARGET_PROVIDES_POWERHAL := true
+# QCRIL
+TARGET_RIL_VARIANT := caf
 
+# Use HW crypto for ODE
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Added to indicate that protobuf-c is supported in this build
+PROTOBUF_SUPPORTED := true
+
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# inherit from the proprietary version
+ifneq ($(QCPATH),)
+-include $(QCPATH)/common/msm8974/BoardConfigVendor.mk
+
+ifeq ($(BOARD_USES_QCNE),true)
+TARGET_LDPRELOAD := libNimsWrap.so
+endif
+endif
+
+# SELinux policies
+# qcom sepolicy
 include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
-    $(COMMON_PATH)/sepolicy
+        device/oppo/msm8974-common/sepolicy
 
-# Recovery:Start
-
-# Use this flag if the board has a ext4 partition larger than 2gb
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_USERIMAGES_USE_EXT4 := true
-
-# TWRP specific build flags
-DEVICE_RESOLUTION := 1080x1920
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_NO_USB_STORAGE := true
-TW_INCLUDE_JB_CRYPTO := false
-TW_NO_SCREEN_BLANK := true
-TW_EXCLUDE_ENCRYPTED_BACKUPS := true
-TW_INCLUDE_L_CRYPTO := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+-include vendor/oppo/msm8974-common/BoardConfigVendor.mk
